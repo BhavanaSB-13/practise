e38d4f20d573a1be461f5df167187947673769a1
BhavanaSB-13 <bhavanabhuvanesh.sb@gmail.com>
2024-10-24 11:05:40 +0530

    feat: Add Scanner class

--- Add.java
+++ Add.java
@@ -1,7 +1,12 @@
+import java.util.*;;
+
 class Add {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         System.out.println(a + b);
+        sc.close();
     }
 }
