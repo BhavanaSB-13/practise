33e98a2cf33072891a285950a022414136675c90
Bhavana S B <130850836+BhavanaSB-13@users.noreply.github.com>
2024-11-04 17:33:51 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+class Calculator{
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter two numbers");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        Add calculator = new Add();
+        System.out.println("Addition: " + calculator.add(a, b));
+        System.out.println("Subtraction: " + calculator.subtract(a, b));
+        System.out.println("Multiplication: " + calculator.multiply(a, b));
+        System.out.println("Division: " + calculator.divide(a, b));
+
+        sc.close();
+    }
+}
