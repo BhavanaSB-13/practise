61a8dbdcacc63bdea5fa461e62434710757f88e3
Bhavana S B <130850836+BhavanaSB-13@users.noreply.github.com>
2024-11-04 17:38:01 +0530

    Update CalciTest.java

--- CalciTest.java
+++ CalciTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class AddTest {
     @Test
     public void testAddition() {
-        Add add = new Add();
+        Calculator add = new Calculator();
         assertEquals(5, add.add(2, 3));
         assertEquals(-1, add.add(-2, 1));
         assertEquals(0, add.add(0, 0));
@@ -13,7 +13,7 @@ public class AddTest {
 
     @Test
     public void testSubtraction() {
-        Add add = new Add();
+         Calculator add = new Calculator();
         assertEquals(-1, add.subtract(2, 3));
         assertEquals(-3, add.subtract(-2, 1));
         assertEquals(0, add.subtract(0, 0));
@@ -22,7 +22,7 @@ public class AddTest {
 
     @Test
     public void testMultiplication() {
-        Add add = new Add();
+        Calculator add = new Calculator();
         assertEquals(6, add.multiply(2, 3));
         assertEquals(-2, add.multiply(-2, 1));
         assertEquals(0, add.multiply(0, 10));
@@ -31,7 +31,7 @@ public class AddTest {
 
     @Test
     public void testDivision() {
-        Add add = new Add();
+        Calculator add = new Calculator();
         assertEquals(2, add.divide(6, 3));
         assertEquals(-2, add.divide(-4, 2));
         assertEquals(0, add.divide(0, 1));
@@ -40,7 +40,7 @@ public class AddTest {
 
     @Test(expected = ArithmeticException.class)
     public void testDivisionByZero() {
-        Add add = new Add();
+         Calculator add = new Calculator();
         add.divide(5, 0);
     }
 }
