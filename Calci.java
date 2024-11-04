4b0185798db3680828933b1b46f3f03eafe251e3
Bhavana S B <130850836+BhavanaSB-13@users.noreply.github.com>
2024-11-04 16:35:51 +0530

    Update Calci.java

--- Calci.java
+++ Calci.java
@@ -1,6 +1,6 @@
 import java.util.*;;
 
-class Add {
+class Calci {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter two numbers");
