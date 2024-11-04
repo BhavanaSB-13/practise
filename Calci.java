7b574002ad529efc5e32937f6575806445c083cd
Bhavana S B <130850836+BhavanaSB-13@users.noreply.github.com>
2024-11-04 17:05:20 +0530

    Update Calci.java

--- Calci.java
+++ Calci.java
@@ -2,8 +2,8 @@ class Calci {
     public static void main(String[] args) {
        
         
-        int a = 12
-        int b =13
+        int a = 12;
+        int b =13;
 
         System.out.println(a+b);
         System.out.println(a-b);
